49592b8cb381cd7155d47b2d3f2b64e4694639a7
Chris <21198609@students.latrobe.edu.au>
2026-08-02 12:27:00 +1000

    created basic pages

---
+++ app/word-search/page.tsx
@@ -0,0 +1,7 @@
+export default function WordSearchPage() {
+  return (
+    <div>
+      <h1>Word-Search</h1>
+    </div>
+  );
+}
---
+++ app/wordle/page.tsx
@@ -0,0 +1,7 @@
+export default function WordlePage() {
+  return (
+    <div>
+      <h1>Wordle</h1>
+    </div>
+  );
+}
